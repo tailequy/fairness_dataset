2f23794cbb8189cc84bc2967408b920abe01d19d
Quy Tai <quytai3985@gmail.com>
2022-04-14 12:54:41 +0200

    Delete acsincome.py

--- experiments/acsincome.py
+++
@@ -1,4 +0,0 @@
-from folktables import ACSDataSource, ACSEmployment
-data_source = ACSDataSource(survey_year='2018', horizon='1-Year', survey='person')
-acs_data = data_source.get_data(states=["CA"], download=True)
-features, label, group = ACSEmployment.df_to_numpy(acs_data)
